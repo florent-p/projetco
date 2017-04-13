2e19aba1d35f74a7ea0d3a62045a77d2b8da43c0
Florent <fpagnot@gmail.com>
2017-04-13 10:23:25 +0200

    12/04

---
+++ app/js/script.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+
+    $('.navbar__menu', this).click(function() {
+        
+        // [MOBILE] Changement de background quand le menu est ouvert
+        $(this).toggleClass('menu--active');
+        
+        var isMenuVisible = $('.nav__menu').is(":visible");
+        if (isMenuVisible) {
+            $('.nav__menu').stop().slideUp();
+        } else {
+            $('.nav__menu').stop().slideDown();
+        }
+        
+    });
+    
+    // [MOBILE] Changement de sens de la flèche de la sous-catégorie du menu quand celle-ci est ouverte
+    $('.menu__sub', this).click(function() {
+       
+        $(this).toggleClass('cat--opened');
+        
+        var subLinks = $(this).find('.sub__links')
+        var isSubMenuVisible = subLinks.is(":visible");
+        if (isSubMenuVisible) {
+            subLinks.stop().slideUp();
+        } else {
+            subLinks.stop().slideDown();
+        }
+        
+    });
+    
+});
+
+
